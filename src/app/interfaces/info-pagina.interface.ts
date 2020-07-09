3adb2e526b867142688ab69229c72fd3ce8062e2
carlos andres fajardo pedraza <carlosafp84@hotmail.com>
2020-07-08 23:53:05 -0500

    Finalizacion Creacion imagenes

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,11 @@
+export interface InfoPagina {
+    titulo?: string;
+    email?: string;
+    nombreCorto?: string;
+    paginaAutor?: string;
+    facebook?: string;
+    twitter?: string;
+    instagram?: string;
+    tublr?: string;
+    equipo_trabajo?: any[];
+}
